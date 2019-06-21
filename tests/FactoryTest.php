7e38a8165292ba620131a2e4234527e91a00525b
mitsuru793 <mitsuru793@gmail.com>
2019-06-21 15:32:23 +0900

    feat: make faker instance persist
    Faker can generate unique value.

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Yahiru\EntityFactory\Tests;
 
 use Faker\Generator;
+use OverflowException;
 use PHPUnit\Framework\TestCase;
 use Yahiru\EntityFactory\Factory;
 
@@ -67,5 +68,32 @@ final class FactoryTest extends TestCase
         $this->assertSame('define1', Factory::of('define1')->make());
         $this->assertSame('define2', Factory::of('define2')->make());
         $this->assertSame('define3', Factory::of('define3')->make());
+    }
+
+
+    public function testGenerateUniqueValue()
+    {
+        Factory::define(
+            'Number',
+            function (array $attr) {
+                return $attr['num'];
+            },
+            function (\Faker\Generator $faker): array {
+                return [
+                    'num' => $faker->unique->randomDigit,
+                ];
+            }
+        );
+
+        $nums = Factory::of('Number')->times(10)->make();
+        $digits = range(0, 9);
+        foreach ($nums as $num) {
+            assert(array_key_exists($num, $digits));
+            unset($digits[$num]);
+        }
+        $this->assertEmpty($digits);
+
+        $this->expectException(OverflowException::class);
+        Factory::of('Number')->times(11)->make();
     }
 }
